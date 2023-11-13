d3d311b6280ee84d2f8451c7596922719071a885
BuildTools <wynessgp@rose-hulman.edu>
2023-11-13 14:47:08 -0600

    Decorator test related stuff

--- src/test/java/domain/DecoratorCheckTests.java
+++ src/test/java/domain/DecoratorCheckTests.java
@@ -5,10 +5,58 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class DecoratorCheckTests {
+
+    // check expects a list of ClassNodes to be passed in, so we'll need to do some setup. 
+    private static List<String> testFiles;
+    private static LintRunner lr;
+
+
+    @BeforeAll 
+    public static void setup() {
+        lr = new LintRunner();
+        CheckStrategy dc = new DecoratorCheck();
+
+        testFiles = new ArrayList<>();
+        testFiles.add("resources.decorator-check.AbstractComponent");
+        testFiles.add("resources.decorator-check.ConcreteComponent");
+        testFiles.add("resources.decorator-check.AbstractComponentDecorator");
+        testFiles.add("resources.decorator-check.ConcreteDecoratorOne");
+        testFiles.add("resources.decorator-check.ConcreteDecoratorTwo");
+
+        lr.addCheck(dc);
+    }
     
     @Test
-    public void decoratorPatternPassesAndHasConcreteClasses() {
+    public void decoratorPatternPasses_HasConcreteClasses_HappyPath() {
+        lr.createClassReaders(testFiles);
+        List<String> res = lr.runChecks().get("Decorator");
+
+        assertEquals(res.get(0), "Potential decorator pattern classes: \n");
+        assertEquals(res.get(1), "\t\tAbstract components: [decorator-check.AbstractComponent] \n");
+        assertEquals(res.get(2), "\t\tConcrete components: [decorator-check.ConcreteComponent] \n");
+        assertEquals(res.get(3), "\t\tAbstract decorators: [decorator-check.AbstractComponentDecorator] \n");
+        assertEquals(res.get(4), "\t\tConcrete decorators: [decorator-check.ConcreteDecoratorOne, decorator-check.ConcreteDecoratorTwo]\n");
+        assertEquals(res.get(5), "Classes that do not participate in the decorator pattern: \n");
+        assertEquals(res.get(6), "\t\t[]\n");
+    }
+
+    @Test
+    public void decoratorPatternPasses_HasConcreteClasses_AbstractComponentLast() {
+
+    }
+
+    @Test
+    public void decoratorPatternPasses_HasConcreteClasses_AbstractDecoratorLast() {
+
+    }
+
+    @Test
+    public void decoratorPatternPasses_NoConreteClasses() {
 
     }
 
@@ -16,14 +64,16 @@ public class DecoratorCheckTests {
     public void decoratorPatternFails_MissingAbstractComponent() {
 
     }
-    
-    
-    
-    
-    
-    
-    
-    private Bruh bruh1;
 
-    private class Bruh {}
+    @Test
+    public void decoratorPatternFails_MissingAbstractDecorator() {
+
+    }
+
+    @Test
+    public void decoratorPatternFails_MissingBothAbstracts() {
+
+    }
+    
+    
 }
