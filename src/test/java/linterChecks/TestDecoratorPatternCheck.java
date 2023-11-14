4aefa4861428a68d56ba6a63501d285561437396
Canon <cm090.tech@gmail.com>
2023-11-13 19:33:42 -0500

    Rename package with test cases

--- src/test/java/linterChecks/TestDecoratorPatternCheck.java
+++ src/test/java/linterChecks/TestDecoratorPatternCheck.java
@@ -1,21 +1,21 @@
-package domain;
+package linterChecks;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import domain.CheckStrategy;
+import domain.DecoratorCheck;
+import domain.LintRunner;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestDecoratorPatternCheck {
-
-    // check expects a list of ClassNodes to be passed in, so we'll need to do some setup. 
+    // check expects a list of ClassNodes to be passed in, so we'll need to do some setup.
     private static List<String> testFiles;
     private static LintRunner lr;
 
-
-    @BeforeAll 
+    @BeforeAll
     public static void setup() {
         lr = new LintRunner();
         CheckStrategy dc = new DecoratorCheck();
@@ -29,7 +29,7 @@ public class TestDecoratorPatternCheck {
 
         lr.addCheck(dc);
     }
-    
+
     @Test
     public void decoratorPatternPasses_HasConcreteClasses_HappyPath() {
         lr.createClassReaders(testFiles);
@@ -38,8 +38,10 @@ public class TestDecoratorPatternCheck {
         assertEquals(res.get(0), "Potential decorator pattern classes: \n");
         assertEquals(res.get(1), "\t\tAbstract components: [decorator-check.AbstractComponent] \n");
         assertEquals(res.get(2), "\t\tConcrete components: [decorator-check.ConcreteComponent] \n");
-        assertEquals(res.get(3), "\t\tAbstract decorators: [decorator-check.AbstractComponentDecorator] \n");
-        assertEquals(res.get(4), "\t\tConcrete decorators: [decorator-check.ConcreteDecoratorOne, decorator-check.ConcreteDecoratorTwo]\n");
+        assertEquals(res.get(3),
+                "\t\tAbstract decorators: [decorator-check.AbstractComponentDecorator] \n");
+        assertEquals(res.get(4),
+                "\t\tConcrete decorators: [decorator-check.ConcreteDecoratorOne, decorator-check.ConcreteDecoratorTwo]\n");
         assertEquals(res.get(5), "Classes that do not participate in the decorator pattern: \n");
         assertEquals(res.get(6), "\t\t[]\n");
     }
@@ -73,6 +75,4 @@ public class TestDecoratorPatternCheck {
     public void decoratorPatternFails_MissingBothAbstracts() {
 
     }
-    
-    
 }
