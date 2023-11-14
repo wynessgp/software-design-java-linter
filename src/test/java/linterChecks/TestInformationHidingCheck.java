20124602a13b6f17ea20e951103d25cf4f3029fc
Canon <cm090.tech@gmail.com>
2023-11-13 20:22:29 -0500

    Create information hiding check tests

---
+++ src/test/java/linterChecks/TestInformationHidingCheck.java
@@ -0,0 +1,63 @@
+package linterChecks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import datasource.RecursiveDiver;
+import datasource.StandardInput;
+import domain.InformationHidingCheck;
+import domain.LintRunner;
+
+public class TestInformationHidingCheck {
+    private static final String TEST_CLASS_DIRECTORY =
+            new File("src/test/resources/information-hiding-check/").getAbsolutePath();
+    private StandardInput reader;
+    private LintRunner runner;
+    private List<String> testFiles;
+
+    @BeforeEach
+    public void reset() {
+        runner = new LintRunner();
+        runner.addCheck(new InformationHidingCheck());
+        testFiles = new ArrayList<>();
+    }
+
+    @Test
+    public void testStaticCheckNoViolationsFound() {
+        // Add test files
+        reader = new RecursiveDiver(new File(TEST_CLASS_DIRECTORY, "success"));
+        while (reader.hasNext()) {
+            testFiles.add(reader.next());
+        }
+        List<String> classPaths = runner.createClassReaders(testFiles);
+        assertEquals(2, classPaths.size());
+
+        // Run and parse checks
+        Map<String, List<String>> results = runner.runChecks();
+        assertEquals(1, results.size());
+        assertEquals(1, results.get("Information hiding").size());
+        assertEquals("No violations detected", results.get("Information hiding").get(0));
+    }
+
+    @Test
+    public void testStaticCheckViolationFound() {
+        // Add test files
+        reader = new RecursiveDiver(new File(TEST_CLASS_DIRECTORY, "fail"));
+        while (reader.hasNext()) {
+            testFiles.add(reader.next());
+        }
+        List<String> classPaths = runner.createClassReaders(testFiles);
+        assertEquals(2, classPaths.size());
+
+        // Run and parse checks
+        Map<String, List<String>> results = runner.runChecks();
+        assertEquals(1, results.size());
+        assertEquals(1, results.get("Information hiding").size());
+        assertEquals("Class MySecondClass has public field myInt",
+                results.get("Information hiding").get(0));
+    }
+}
